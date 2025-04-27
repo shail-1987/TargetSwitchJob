0eb124c7b9f1efbfb9801047015cbad5670c7aba
Shailendra Prakash <prakashshailendra45@gmail.com>
2025-04-27 12:54:11 +0530

    update

--- src/main/java/com/java/practice/BasicsOfJava.java
+++ src/main/java/com/java/practice/BasicsOfJava.java
@@ -12,6 +12,14 @@ public class BasicsOfJava {
 	System.out.println(a[3]);
 	for(int b:a) {
 		System.out.println(b);
-	}
+	}}
+	public static void addTwoNumbers1() {
+		int a[]= {4,6,8,7};
+		System.out.println(a.length);
+		System.out.println(a[3]);
+		for(int b:a) {
+			System.out.println(b);
+		}
+
 	}
 }
